ef40361c58bd053593cb7a1c318d8c31a666a952
Aman Jifry <z5592111@ed.unsw.edu.au>
2025-09-19 21:04:25 +1000

    finished exercise 1

---
+++ lab1_tests.cpp
@@ -0,0 +1,26 @@
+// Exercise 1
+// #include <iostream>
+
+// int main() {
+//     std::cout << "Hello World!\n";
+//     return 0;
+// }
+
+// Exercise 2
+#include <iostream>
+
+int mult() {
+    float a{0.0};
+    float b{0.0};
+    float c{0.0};
+    std::cin >> a;
+    std::cin >> b;
+    c = a * b;
+    std::cout << static_cast<int>(c) <<"\n";
+    return 0;
+}
+
+int main() {
+    mult();
+    return 0;
+}
